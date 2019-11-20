81a3a0bccf3ff0667ca16675915940a1e23a5428
Daphne <daphne.feresin@gmail.com>
2019-11-20 17:50:54 -0500

    s

--- src/components/GuruAnswerDesign.js
+++ src/components/GuruAnswerDesign.js
@@ -8,7 +8,7 @@ export class GuruAnswerDesign extends Component {
             <>
                <section className="answer-section">
                 <img className="guru-img2" src="./images/guru2.png"/> 
-               <img className="speech-image" src="./images/speech-bubble.png"/>
+                <img className="speech-image" src="./images/speech-bubble.png"/>
               </section>
             </>
         )
