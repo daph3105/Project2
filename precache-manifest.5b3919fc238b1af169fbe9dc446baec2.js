96601766ba760c54b34ae5afbc35afa2557c0ab6
Daphne <daphne.feresin@gmail.com>
2019-11-20 14:55:17 -0500

    Updates

---
+++ precache-manifest.5b3919fc238b1af169fbe9dc446baec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0f798d0d8d470b354adaed602de7d1",
+    "url": "/Project2/index.html"
+  },
+  {
+    "revision": "93d8cd5c49a96587a855",
+    "url": "/Project2/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "6da816a0eca38d825879",
+    "url": "/Project2/static/css/main.6aca3a52.chunk.css"
+  },
+  {
+    "revision": "93d8cd5c49a96587a855",
+    "url": "/Project2/static/js/2.06882633.chunk.js"
+  },
+  {
+    "revision": "6da816a0eca38d825879",
+    "url": "/Project2/static/js/main.826ec565.chunk.js"
+  },
+  {
+    "revision": "3861ec6353be967ba94d",
+    "url": "/Project2/static/js/runtime-main.4c5f532a.js"
+  }
+]);
