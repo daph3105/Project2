82b5c9fb245cd5b38ac6a41acad8814ff2e131cd
Daphne <daphne.feresin@gmail.com>
2019-11-20 17:45:50 -0500

    Updates

--- precache-manifest.c6f6899aa062b7a8f0d26623cff6a436.js
+++ precache-manifest.c6f6899aa062b7a8f0d26623cff6a436.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea595877e809543e00452513d74c4632",
+    "revision": "a130f3557fabac429e39498db82cf6bc",
     "url": "/Project2/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Project2/static/css/2.833dd627.chunk.css"
   },
   {
-    "revision": "185222821211df7a1fef",
+    "revision": "d4dc400aae8ecd5cd264",
     "url": "/Project2/static/css/main.3edd500d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Project2/static/js/2.b6e97d01.chunk.js"
   },
   {
-    "revision": "185222821211df7a1fef",
-    "url": "/Project2/static/js/main.a02d9d52.chunk.js"
+    "revision": "d4dc400aae8ecd5cd264",
+    "url": "/Project2/static/js/main.d7c1115d.chunk.js"
   },
   {
     "revision": "3861ec6353be967ba94d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Project2/precache-manifest.28e0d3e21d43dd52ebc7cafe95327099.js"
+  "/Project2/precache-manifest.c6f6899aa062b7a8f0d26623cff6a436.js"
 );
 
 self.addEventListener('message', (event) => {
