335a6e004892a2b34a678489995d1790bc9622c3
Daphne <daphne.feresin@gmail.com>
2019-11-24 00:30:14 -0500

    home-buttons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,10 +19,10 @@ export class HomePage extends Component {
 
             <div className="home">
             <section className="home-buttons">
-            <button className="btn btn-danger" style={{height:"70px"}}><Link to="/yes-no" style={{textDecoration:"none", color:"white"}}>Yes / No Questions</Link></button>
-            <button className="btn btn-danger" style={{height:"70px"}}><Link to="/this-that" style={{textDecoration:"none", color:"white"}}>This or That Questions</Link></button>
-            <button className="btn btn-danger" style={{height:"70px"}}><Link to="/decide" style={{textDecoration:"none", color:"white"}}>Let the Guru decide things for you</Link></button>
-            <button className="btn btn-danger" style={{height:"70px"}}><Link to="/advice" style={{textDecoration:"none", color:"white"}}>Ask the Guru for an Advice</Link></button>
+            <Link to="/yes-no" style={{textDecoration:"none", color:"white"}}><button className="btn btn-danger"> Yes / No Questions</button></Link>
+            <Link to="/this-that" style={{textDecoration:"none", color:"white"}}><button className="btn btn-danger"> This or That Questions</button></Link>
+            <Link to="/decide" style={{textDecoration:"none", color:"white"}}><button className="btn btn-danger"> Let the Guru decide things for you</button></Link>
+            <Link to="/advice" style={{textDecoration:"none", color:"white"}}><button className="btn btn-danger"> Ask the Guru for an Advice</button></Link>
             </section> 
                 <img className="guru-img-home" src="./images/guru1.png"/>
             </div>
