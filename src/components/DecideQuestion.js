bdb128c2d586b33a86232b7328b27da041ffacd4
Daphne <daphne.feresin@gmail.com>
2019-11-26 15:13:09 -0500

    mobile updates

--- src/components/DecideQuestion.js
+++ src/components/DecideQuestion.js
@@ -33,14 +33,14 @@ export class decideQuestion extends Component {
             <>
             <NavDesktop/>
             <section className="question-section">
-            <img className="guru-img-question" src="./images/guru1.png"/>
+            <img className="guru-img-question" id="decide-guru" src="./images/guru1.png"/>
 
             <div className="baloon-container-decide">
-            <Link to="/answer3"> <input id="baloon1" type="image" src="./images/questionimg6.png" alt="out tonight" width="238" height="208"/> </Link>
-            <Link to="/eat"> <input id="baloon2" type="image" src="./images/questionimg7.png" alt="what to eat" width="248" height="208"></input> </Link>
-            <Link to="/answer8"> <input id="baloon3" type="image" src="./images/questionimg8.png" alt="what to watch" width="258" height="208"></input>  </Link>
-            <Link to="/answer9"> <input id="baloon4" type="image" src="./images/questionimg9.png" alt="what to listen" width="258" height="208"></input>  </Link>
-            <Link to="/answer7"> <input id="baloon5" type="image" src="./images/questionimg10.png" alt="what to do" width="258" height="208"></input> </Link>
+            <Link to="/answer3"> <input id="baloon1" type="image" src="./images/questionimg6.png" alt="out tonight"/> </Link>
+            <Link to="/eat"> <input id="baloon2" type="image" src="./images/questionimg7.png" alt="what to eat"/> </Link>
+            <Link to="/answer8"> <input id="baloon3" type="image" src="./images/questionimg8.png" alt="what to watch"/></Link>
+            <Link to="/answer9"> <input id="baloon4" type="image" src="./images/questionimg9.png" alt="what to listen"/></Link>
+            <Link to="/answer7"> <input id="baloon5" type="image" src="./images/questionimg10.png" alt="what to do"/></Link>
             </div>
 
             {/* <Dropdown show={this.state.show} value={this.state.value} handleToggle={this.handleToggle} 
--- src/components/YesNoQuestion.js
+++ src/components/YesNoQuestion.js
@@ -49,21 +49,21 @@ myChangeHandler = (event) => {
             <>
              <NavDesktop/>
             <section className="question-section">
-            <img className="guru-img-question" src="./images/guru1.png"/>
+            <img className="guru-img-question" id="yes-noimg" src="./images/guru1.png"/>
 
-            <h3>Type your Yes/No question below,</h3>
-            <p>or click on one of the frequently asked questions to ask the Guru.</p>
+            <h3 id="yes-no-title">Type your Yes/No question below,</h3>
+            <p id="yes-noP">or click on one of the frequently asked questions to ask the Guru.</p>
             <div>
             <input id="yes-no-question" type="text" name="yes-no-question" placeholder= "Guru..." value = {this.state.question} onChange = {this.myChangeHandler}/>
             <Link to="/answer1" style={{textDecoration:"none", color:"white"}}><button className= "btn btn-danger" id="btn-question" type = "submit"> Ask </button></Link>
             </div>
             </section> 
             <div className="baloon-container">
-            <Link to="/answer1"> <input id="baloon1" type="image" src="./images/questionimg1.png" alt="find love" width="238" height="208"/> </Link>
-            <Link to="/answer1"> <input id="baloon2" type="image" src="./images/questionimg2.png" alt="quit job" width="248" height="208"></input> </Link>
-            <Link to="/answer1"> <input id="baloon3" type="image" src="./images/questionimg3.png" alt="be rich" width="258" height="208"></input>  </Link>
-            <Link to="/answer1"> <input id="baloon4" type="image" src="./images/questionimg4.png" alt="stay or go" width="258" height="208"></input>  </Link>
-            <Link to="/answer1"> <input id="baloon5" type="image" src="./images/questionimg5.png" alt="send text" width="258" height="208"></input> </Link>
+            <Link to="/answer1"> <input id="baloon1" type="image" src="./images/questionimg1.png" alt="find love"/> </Link>
+            <Link to="/answer1"> <input id="baloon2" type="image" src="./images/questionimg2.png" alt="quit job"/> </Link>
+            <Link to="/answer1"> <input id="baloon3" type="image" src="./images/questionimg3.png" alt="be rich"/> </Link>
+            <Link to="/answer1"> <input id="baloon4" type="image" src="./images/questionimg4.png" alt="stay or go"/> </Link>
+            <Link to="/answer1"> <input id="baloon5" type="image" src="./images/questionimg5.png" alt="send text"/> </Link>
             </div>
            
            
