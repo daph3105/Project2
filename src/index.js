eb927dd20acc609dcf02c513f5001a0faaf7fbe4
Daphne <daphne.feresin@gmail.com>
2019-11-27 09:25:03 -0500

    cursor update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 console.log('hi')
